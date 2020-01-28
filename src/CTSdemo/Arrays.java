a82a479a8ca2912259215ab9a96a06d9de7486e1
AnjaliChintham <anjalichintham98@gmail.com>
2020-01-28 14:22:01 +0530

    Added readme

---
+++ src/CTSdemo/Arrays.java
@@ -0,0 +1,15 @@
+package CTSdemo;
+import java.util.Scanner;
+
+public class Arrays {
+	public static void main(String[]args)
+	
+	{  int i;
+      int[] age= {1,12,234,3456,4567};
+      for(i=0;i<5;i++)  	  
+      	
+     System.out.println(age[i]);
+	
+	
+}
+}
